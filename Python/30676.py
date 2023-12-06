a0b23a22095cba54b5e308011e892140bdaea64b
Dogyeom Kim <91599095+zorocrit@users.noreply.github.com>
2023-12-06 23:16:24 +0900

    Create 30676.py

---
+++ Python/30676.py
@@ -0,0 +1,15 @@
+aa = int(input())
+if(aa <= 780 and aa >= 620):
+    print("Red")
+elif(aa >= 590 and aa < 620):
+    print("Orange")
+elif(aa >= 570 and aa < 590):
+    print("Yellow")
+elif(aa >= 495 and aa < 570):
+    print("Green")
+elif(aa >= 450 and aa < 495):
+    print("Blue")
+elif(aa >= 425 and aa < 450):
+    print("Indigo")
+elif(aa >= 380 and aa < 425):
+    print("Violet")
