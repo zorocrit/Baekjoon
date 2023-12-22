f939e4534d4cf622219e3f8e89534d33ec82bc3a
Dogyeom Kim <91599095+zorocrit@users.noreply.github.com>
2023-12-22 22:30:20 +0900

    Create 25314.cc

---
+++ C_Lang/25314.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nn;
+    cin >> nn;
+    int aa = nn / 4;
+    while(aa){
+        cout << "long ";
+        aa--;
+    }
+    cout << "int";
+    return 0;
+}
